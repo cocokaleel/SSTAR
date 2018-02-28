66b84889b5bc26376edcfa60c579e6694585a404
Caroline Kaleel <cocokaleel@gmail.com>
2018-02-27 16:41:14 -0800

    Level 1 Alterations
    Added Level1.jpg to folder, worked on main.js and Level1.html, attempted to add image and relate the two files to each other

--- WebContent/Level 1/main.js
+++ WebContent/Level 1/main.js
@@ -1,3 +1,12 @@
 /**
- * Hello
+ * This is the JS file that has the code for the LEVEL 1
  */
+
+function showImage() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "Level1.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "Level1");
+    document.body.appendChild(x);
+}
